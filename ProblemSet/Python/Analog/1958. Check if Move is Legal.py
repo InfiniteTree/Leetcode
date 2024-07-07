867db9fd61d9e1bfe0703b56117ec5da73f0e613
InfiniteTree <yuhangc128@gmail.com>
2024-07-07 20:47:02 +0800

    analog

---
+++ ProblemSet/Python/Analog/1958. Check if Move is Legal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkMove(self, board: List[List[str]], rMove: int, cMove: int, color: str) -> bool:
+        m, n = len(board), len(board[0])
+        for dx, dy in (0, 1), (0, -1), (1, 1), (1, 0), (1, -1), (-1, 0), (-1, -1), (-1, 1):
+            x, y = rMove + dx, cMove + dy
+            if x < 0 or x >= m or y < 0 or y >= n or board[x][y] == "." or board[x][y] == color:
+                continue
+            while True:
+                x += dx
+                y += dy
+                if x < 0 or x >= m or y < 0 or y >= n or board[x][y] == ".":
+                    break
+                if board[x][y] == color:
+                    return True
+        return False
+        
